c92babc8ccbac069ba46f14148a369e69c561177
Andrew Rogala <adrogala87@gmail.com>
2018-09-05 18:39:32 -0400

    hero video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import styles from './index.css';
+
+import videoWEBM from '../img/Hello-World.webm';
+import videoMP4 from '../img/Hello-World.mp4';
 
 export default () => (
 	<main role="main">
-		<h3>Featured Work</h3>
+		<section className="hero">
+			<div className="bg-video">
+				<video className="bg-video__content" autoplay muted loop>
+					<source src={videoMP4} type="video/mp4" />
+					<source src={videoWEBM} type="viedo/webm" />
+				</video>
+			</div>
+			<div className="hero__text-box">
+				<h3 className="heading-tertiary1">Responsive design since 2018</h3>
+			</div>
+		</section>
 	</main>
 );
