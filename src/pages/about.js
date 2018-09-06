083b315dc223097bda9ba17f9cd829f20361e2e2
Andrew Rogala <adrogala87@gmail.com>
2018-09-06 10:39:45 -0400

    add about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 export default () => (
-	<div>
-		<h3>About Me</h3>
-	</div>
+	<main>
+		<section className="featured-work">
+			<div className="featured-work__description-1" style={{width: '70%'}}>
+				<h3 className="heading-tertiary3">Welcome</h3>
+				<p className="featured-work__description-p">Hi I am glad you found me. I first discovered my interest in programming
+				and computer science in 2014 during two semesters of Java at Montclair State University.
+				Since then I have never really stopped learning about programming and computers.
+				I have tinkered with Python and the Raspberry Pi, learned some statistical programming with R in a data mining course,
+				and even picked up a little Excel VBA at work to automate some calculations.<br/><br/>
+				Recently, I have been lucky enough to take this hobby of mine and pursue it full time.
+				I have chosen to focus on Web Development and am working hard toward full stack development.
+				I am drawn to Web development because not only do I enjoy the front-end design, UI and UX aspect
+				but I am also very technical and love solving complex problems;
+				as such I look forward to diving into server-side programming as well.
+				It just feels like the right mix of art and technology for me.<br/><br/>
+				I am currently focused on front-end technologies which includes HTML, CSS with SASS, and of course JavaScript.
+				I am not sure on which server-side language I will choose to specialize in but have a feeling I will learn
+				both Node.js and PHP (check out this interesting article on the two languages
+				<a href="https://www.infoworld.com/article/3166109/application-development/php-vs-nodejs-an-epic-battle-for-developer-mind-share.html">PHP vs NodeJS</a>
+				</p>
+			</div>
+		</section>
+	</main>
 );
