280f710dde9e20b4b79319892fb72035e2af7224
Andrew Rogala <adrogala87@gmail.com>
2018-09-06 09:51:24 -0400

    update project descriptions

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -17,7 +17,14 @@ export default () => (
 		<div className="featured-work__description-1">
 			<h3 className="heading-tertiary3">Frogger Clone</h3>
 			<a href="https://andrewrogalafrogger.surge.sh/" className="featured-work__link">Check out my game</a>
-			<p className="featured-work__description-p"></p>
+			<p className="featured-work__description-p">Zombie Squash was originally coded for Udacity's Front End Nandodegree program.
+			It demonstrates use of objcet oriented programming in JavaScript. I had a lot of fun building this game and even learned a bit
+			about basic game engine design in the process. I added lots of addition feateures that were not part of the original
+			specification in order to sharpen my skills and make the project uniquely mine.
+			Some of the extra features inclue: multiple types of eniemies, sound effects, music, and a bigger tile board!! Use the arrow or wasd
+			keys to move your player safely accross the board and eat some brains!! Sorry NO mobile phone support on this game.
+			The code and full list of game play rules are on github:
+			<a href="https://github.com/ARogala/Frogger"> Frogger Clone</a></p>
 		</div>
 
 		<div className="featured-work__project-2">
@@ -40,8 +47,7 @@ export default () => (
 		<div className="featured-work__description-3">
 			<h3 className="heading-tertiary3">JavaScript Timer</h3>
 			<a href="https://codepen.io/Drew7865/pen/EErMvL" className="featured-work__link">Check out my timer on Code Pen</a>
-			<p className="featured-work__description-p">Check out my JavaScript timer on CodePen.
-			I wrote this timer to demonstrate knowledge of the JavaScript language.
+			<p className="featured-work__description-p">I wrote this timer to demonstrate knowledge of the JavaScript language.
 			Specifically, this timer has been coded with the modular design pattern and makes use of
 			DOM manipulation in vanilla JavaScript. I plan to add this to my home page project as well as
 			updating some of the timer’s CSS.</p>
@@ -49,17 +55,28 @@ export default () => (
 
 		<div className="featured-work__description-4">
 			<h3 className="heading-tertiary3">JavaScript Stop Watch</h3>
-			<a href="" className="featured-work__link">Check out my stop watch on Code Pen</a>
-			<p className="featured-work__description-p">Check out my JavaScript stop watch on CodePen.
-			I wrote this stop watch to demonstrate knowledge of the JavaScript language.
+			<a href="https://codepen.io/Drew7865/pen/jvGVdg" className="featured-work__link">Check out my stop watch on Code Pen</a>
+			<p className="featured-work__description-p">I wrote this stop watch to demonstrate knowledge of the JavaScript language.
 			This stop watch has been coded with the modular design pattern and makes use of
-			DOM manipulation in vanilla JavaScript.</p>
+			DOM manipulation in vanilla JavaScript. I spent a fair about of time on this code and tried my best to make the code clean
+			and well commented. I have integrated this code into both of my games and hope you can make use of it too. I built this before I was
+			useing git but I will post this on github as well. I may rewrite this in React.</p>
 		</div>
 
 		<div className="featured-work__description-5">
 			<h3 className="heading-tertiary3">Feed Reader</h3>
-			<a href="" className="featured-work__link">Check out my feed reader on github</a>
-			<p className="featured-work__description-p">Check out my feed reader on github.</p>
+			<a href="https://github.com/ARogala/ClientSideFeedReader" className="featured-work__link">Check out my feed reader on github</a>
+			<p className="featured-work__description-p">I developed Client Side Feed Reader (CSFeedy) as a fun and productive way
+			to learn about AJAX in native JavaScript. Despite its name this single page web app isn’t 100% Client Side as it utilizes
+			rss2json.com API for converting the RSS and Atom feeds to usable JSON objects.
+			Everything else is handled on the client side with JavaScript, CSS, and HTML (BEM naming convention is used).
+			All the feeds (and API Key) input get stored in the web browsers local storage.
+			The feeds can be backed up and restored in order to keep the data persistent in the event of clearing browser data.
+			This app only works in Chrome and Firefox. I will admit this project still need a lot of UI UX work as well as
+			obvious error handleing and an upgrade to the fetch api. However, there is still some good reusable code in there and I
+			really like the concept of the app. Who wants to sign up for yet another service just to get some RSS feeds??
+			I actually plan to rewrite this in React, competely remove
+			the rss2json dependency, hopefully make it mobile frendly, and host in with Surge.</p>
 		</div>
 
 	</section>
