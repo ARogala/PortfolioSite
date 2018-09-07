7d8027220ad658f25253999d215e2cfb6b18cbcc
Andrew Rogala <adrogala87@gmail.com>
2018-09-07 09:42:30 -0400

    add meta description

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -22,6 +22,7 @@ const Layout = ({children, data}) => (
 			<title>{data.site.siteMetadata.title}</title>
 			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous"/>
 			<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet"/>
+			<meta name="description" content="Portfolio site for web developer Andrew Rogala" />
 		</Helmet>
 		<nav className="nav">
 			<input type="checkbox" className="nav__checkbox" id="navi-toggle" />
